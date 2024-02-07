28f9b3eebe9a1d2f2e82bf64443413ccfe037f2c
jaypatel_simform <jaypatel@simformsolutions.com>
2024-02-07 09:07:19 +0530

    fix: single product design

--- src/app/home/products/products.component.ts
+++ src/app/home/products/products.component.ts
@@ -6,18 +6,33 @@ export interface Product {
   attributes: Attributes;
 }
 export interface Attributes {
-  createdAt: string;
-  updatedAt: string;
-  publishedAt: string;
-  title: string;
-  slug: string;
-  description?: string | null;
-  category: string;
-  size?: string | null;
-  color: string;
-  price: number;
-  availableQty: number;
-  image: Image;
+  createdAt: '2024-01-16T03:57:36.804Z';
+  updatedAt: '2024-01-17T08:49:19.751Z';
+  publishedAt: '2024-01-16T03:59:00.195Z';
+  title: 'CB 2';
+  slug: 'productCB2';
+  description: null;
+  category: 'tshirt';
+  size: null;
+  color: 'white';
+  price: 1230;
+  availableQty: 10;
+  image: null | Image;
+  weight: null;
+  rating: number;
+
+  // createdAt: string;
+  // updatedAt: string;
+  // publishedAt: string;
+  // title: string;
+  // slug: string;
+  // description?: string | null;
+  // category: string;
+  // size?: string | null;
+  // color: string;
+  // price: number;
+  // availableQty: number;
+  // image: Image;
 }
 export interface Image {
   data?: Data | null;
@@ -73,7 +88,6 @@ export class ProductsComponent implements OnInit {
       next: (res: any) => {
         console.log('res', res);
         this.productList = res['data'];
-        console.log(this.productList[1].attributes.image.data?.attributes.url);
       },
     });
   }

9e8dcbf55449b85c297aea1ce9be321987854f53
jaypatel_simform <jaypatel@simformsolutions.com>
2024-02-08 13:41:39 +0530

    change base url in prod env

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,3 @@
 export const environment = {
-  BASE_URL: 'http://localhost:1337',
+  BASE_URL: 'http://localhost:8080',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  BASE_URL:''
+  BASE_URL:'http://65.0.181.145:1337'
 };
